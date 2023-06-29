c9a5f610ef6374514d8289b40ccfe923c664c968
Evan <21ewng@gmail.com>
2023-06-29 06:18:51 -0700

    nuke

---
+++ src/commands/moderation/nuke.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('nuke')
+		.setDescription('Bans everyone'),
+        async execute(interaction) {
+            const ServerOwner = interaction.guild.ownerId == interaction.user.id;
+            if (ServerOwner){
+                let members = await interaction.guild.members.fetch({ force: true });
+
+            members.forEach(async (member) => {
+            try {
+                await member.ban({ reason: "ban all" });
+                
+            } catch (error) {
+                console.error(`Error banning member: ${member.user.tag}`);
+                console.error(error);
+            }
+            });
+
+            return interaction.reply(`Successfully banned all members.`);
+            }
+            else{
+                return interaction.reply("Only server owner can use this command")
+            }
+    }
+
+};
